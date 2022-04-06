09d332121f592a95eff866a5fd3732bf97fba1f2
Debarshi Das <debarshi.das2019@vitstudent.ac.in>
2022-04-06 17:05:04 +0530

    adding bars

--- js/insertion.js
+++ js/insertion.js
@@ -1,11 +1,20 @@
+const container = document.querySelector('.content');
+
+
 function insertion_start () {
   
-  let bars = [];
-  let bars_count = 0;
+  let bars = []; 
   
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) 
+  {
     bars[i] = Math.floor(Math.random() * 101);
+    
+
+
+
+
   }
+
   
-  
+
 }
